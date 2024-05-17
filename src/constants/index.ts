02fb1c5637ce51ceb0c3bc35f940099a8cbdd59c
AlanYsc <alanysc@qq.com>
2024-05-18 07:08:21 +0800

    Initial commit

---
+++ src/constants/index.ts
@@ -0,0 +1,6 @@
+export const SYSTEM_LOGO = "https://alany.oss-cn-hangzhou.aliyuncs.com/img/wx.jpg";
+
+/**
+ * 知识星球文档地址
+ */
+export const PLANET_LINK = "https://github.com/AlanYsc6";
---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import { PageContainer } from '@ant-design/pro-layout';
+import {Typography,Image} from 'antd';
+import styles from './Welcome.less';
+import {SYSTEM_LOGO} from '@/constants';
+
+const CodePreview: React.FC = ({ children }) => (
+  <pre className={styles.pre}>
+    <code>
+      <Typography.Text copyable>{children}</Typography.Text>
+    </code>
+  </pre>
+);
+const Wx: React.FC = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <Image
+        alt={'WeiXin'}
+        width={300}
+        src={SYSTEM_LOGO}
+      />
+    </div>
+  );
+};
+const Welcome: React.FC = () => {
+  return (
+    <PageContainer>
+      <Wx/>
+      {/*<Card>*/}
+      {/*  <Alert*/}
+      {/*    message={'更快更强的重型组件，已经发布。'}*/}
+      {/*    type="success"*/}
+      {/*    showIcon*/}
+      {/*    banner*/}
+      {/*    style={{*/}
+      {/*      margin: -12,*/}
+      {/*      marginBottom: 24,*/}
+      {/*    }}*/}
+      {/*  />*/}
+      {/*  <Typography.Text strong>*/}
+      {/*    高级表格{' '}*/}
+      {/*    <a*/}
+      {/*      href="https://procomponents.ant.design/components/table"*/}
+      {/*      rel="noopener noreferrer"*/}
+      {/*      target="__blank"*/}
+      {/*    >*/}
+      {/*      欢迎使用*/}
+      {/*    </a>*/}
+      {/*  </Typography.Text>*/}
+      {/*  <CodePreview>yarn add @ant-design/pro-table</CodePreview>*/}
+      {/*  <Typography.Text*/}
+      {/*    strong*/}
+      {/*    style={{*/}
+      {/*      marginBottom: 12,*/}
+      {/*    }}*/}
+      {/*  >*/}
+      {/*    高级布局{' '}*/}
+      {/*    <a*/}
+      {/*      href="https://procomponents.ant.design/components/layout"*/}
+      {/*      rel="noopener noreferrer"*/}
+      {/*      target="__blank"*/}
+      {/*    >*/}
+      {/*      欢迎使用*/}
+      {/*    </a>*/}
+      {/*  </Typography.Text>*/}
+      {/*  <CodePreview>yarn add @ant-design/pro-layout</CodePreview>*/}
+      {/*</Card>*/}
+
+    </PageContainer>
+  );
+};
+
+
+export default Welcome;
